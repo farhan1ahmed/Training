b1e7f8032b5f4dce810c93137569f4fe1c1bc6e4
Farhan Ahmed <farhan1ahmed@hotmail.com>
2020-01-19 14:57:56 +0500

    Week2 Day1 Task3

--- Week2Day1/Abstract Classes.py
+++ Week2Day1/Abstract Classes.py
@@ -1,5 +1,5 @@
-from abc import abstractmethod
-class Book:
+from abc import ABC, abstractmethod
+class Book(ABC):
     def __init__(self, title, author, price):
         self.title = title
         self.author = author
