2de54e7680a4c479623c121987989305b643119f
Farhan Ahmed <farhan1ahmed@hotmail.com>
2020-01-19 12:50:29 +0500

    Week2 Day1 Task2

---
+++ Week2Day1/Inheritance.py
@@ -0,0 +1,24 @@
+class Cls1:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+
+class Cls2(Cls1):
+    def multiply(self):
+        return self.a*self.b
+
+    def taks(self):
+        return pow(self.a, 2) + pow(self.b, 2)
+
+
+cases = int(input("Enter number of tries: "))
+for case in range(cases):
+    numbers = input().split()
+    obj = Cls2(int(numbers[0]), int(numbers[1]))
+    print(obj.add())
+    print(obj.multiply())
+    print(obj.taks())
