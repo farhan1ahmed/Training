c3864cba694d1c529130adeec78a6f25de73823a
Farhan Ahmed <farhan1ahmed@hotmail.com>
2020-02-09 16:09:52 +0500

    Week3 Day1+2 Sorting Task5

---
+++ Week3Day1+2/Counting Sort 2.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingSort function below.
+def countingSort(arr):
+    largest = max(arr)
+    countarr = [0] * (largest + 1)
+    for i in range(len(arr)):
+        countarr[arr[i]] += 1
+    for i in range(len(countarr)):
+        for j in range(countarr[i]):
+            print(i, end= ' ')
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = [int(x) for x in input().split()]
+
+    countingSort(arr)
+
